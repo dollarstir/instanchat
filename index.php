63fed015acf13c0d0c71f1202fd25c1cecb370e2
Frederick Ennin <kpin463@gmail.com>
2023-09-29 03:24:57 +0800

    sdsdf

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 
 
-    // use Twilio\Rest\Client;
+    use Twilio\Rest\Client;
 
     
     require 'config.php';
@@ -12,7 +12,7 @@ require 'vendor/autoload.php';
     // and set the environment variables. See http://twil.io/secure
     $sid = "AC283146a35a1eb01abbc41967c54864bd";
     $token = "8836 f8c16aafbd4c3ac2d18f7190ad6c";
-    $twilio = new  Twilio\Rest\Client($sid, $token);
+    $twilio = new Client($sid, $token);
     
     $message = $twilio->messages
     ->create("+12349782415", // to
@@ -21,18 +21,18 @@ require 'vendor/autoload.php';
     [
         "body" => "Working With mms",
         "from" => "+18447261728",
-        "mediaUrl" => ["https://bydayjobafrica.com/storage/auth/login/IGiCLROPbiZVXK1xtVHPpzcIxRcVYlJm7Sxtm9P7.jpg"]
+        // "mediaUrl" => ["https://bydayjobafrica.com/storage/auth/login/IGiCLROPbiZVXK1xtVHPpzcIxRcVYlJm7Sxtm9P7.jpg"]
         ]
     );
     
     print($message->sid);
 
-    $sql = "INSERT INTO `messages`(`msg_body`, `msg_from`, `msg_to`, `date_created`) VALUES ('Working With mms','+18447261728','+233556676471','".date('Y-m-d H:i:s')."')";
-    $query = mysqli_query($conn, $sql);
-    if($query){
-        echo "Message Sent";
-    }else{
-        echo "Message Not Sent";
-    }
+    // $sql = "INSERT INTO `messages`(`msg_body`, `msg_from`, `msg_to`, `date_created`) VALUES ('Working With mms','+18447261728','+233556676471','".date('Y-m-d H:i:s')."')";
+    // $query = mysqli_query($conn, $sql);
+    // if($query){
+    //     echo "Message Sent";
+    // }else{
+    //     echo "Message Not Sent";
+    // }
     
     
